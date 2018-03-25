67628ff21e7edad7dfcbfc3ccd6dbfbf476874ea
LucasRoKK <37778908+LucasRoKK@users.noreply.github.com>
2018-03-25 15:04:22 -0300

    Add files via upload

---
+++ Aluno.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_3
+{
+    class Aluno
+    {
+        public string nome;
+        public double nota1, nota2, nota3, notafinal;
+
+        public void SomaDasNotas()
+        {
+            notafinal = nota1 + nota2 + nota3;
+        }
+
+        public void Aprovacao()
+        {
+            if(notafinal >= 60.00)
+            {
+                Console.WriteLine("NOTA FINAL: {0}", notafinal);
+                Console.WriteLine("AROVADO!!");
+                Console.ReadLine();
+            }
+            else
+            {
+                double val;
+                val = 60.00 - notafinal;
+                Console.WriteLine("NOTA FINAL: {0}", notafinal);
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("Faltaram "+ val + " pontos");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("---- ESCOLA DO VITORIA RÉGIA----");
+            Console.WriteLine();
+            Aluno alu = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            alu.nome = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Digite a primeira nota: ");
+            alu.nota1 = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Digite a segunda nota: ");
+            alu.nota2 = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Digite a terceira nota: ");
+            alu.nota3 = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            alu.SomaDasNotas();
+
+            alu.Aprovacao();
+        }
+    }
+}
